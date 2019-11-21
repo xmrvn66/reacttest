8c6de4894f194a88718d67738f1ee3a7085dfca2
Marvin Kopietz <marvinkopietz@yahoo.de>
2019-11-21 19:56:27 +0100

    Build React App 1
    App 1

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -5,8 +5,8 @@ const Card = (props) => {
     return ( 
     
     <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img alt = 'robot'
-        src = "https://robohash.org/test1?200x200" />
+        <img alt = 'robots'
+        src ={`https://robohash.org/${props.id}?200x200`} />
         <div>
         <h2 > {props.name} </h2> 
         <p> {props.email} </p> 
